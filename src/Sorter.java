50188b86b566b3a465d26aa8a2f3623be2e936cb
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-11-09 21:45:29 +0100

    commented initial to-do list

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,3 +1,13 @@
 public class Sorter {
+    // create method for sorting unsorted list
+    // return sorted list
+    // int[] alist = {76, 11, -11, 2, 16, -21, 7, 88};
+    public static int[] sort(int[] unsorted) {
+        int[] sorted;
+
+        return sorted;
+
+        
+    }
     
 }
