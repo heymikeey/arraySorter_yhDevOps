12bb6991de6be10d82127c8df9eaa6a4ee0455aa
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-11-09 21:44:46 +0100

    commented initial to-do list

--- src/SortMain.java
+++ src/SortMain.java
@@ -1,5 +1,8 @@
-public class Mainnnpi {
+public class SortMain {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // convert String[] to int[]
+        // create method to call Sorter with unsorted list
+        // save sorted list in variable
+        // crate method to print sorted list in console
     }
 }
