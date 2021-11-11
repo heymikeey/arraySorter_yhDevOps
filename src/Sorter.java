f1e4ecd58ae988ecec90e591f4f9ba3868d955ee
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-11-11 11:28:41 +0100

    just a test commit. Please ignore

--- src/Sorter.java
+++ src/Sorter.java
@@ -2,8 +2,9 @@ public class Sorter {
     // create method for sorting unsorted list
     // return sorted list
     // int[] alist = {76, 11, -11, 2, 16, -21, 7, 88};
+    // comment for commit test /git /github .INGORE
     public static int[] sort(int[] unsorted) {
-        int[] sorted;
+        
 
         return sorted;
 
