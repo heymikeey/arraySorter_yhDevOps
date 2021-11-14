b745088ab9e5ef079e9604be0d52a2a20cc56653
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-11-14 12:57:46 +0100

    string[] converts to int[]. Sorted array print in terminal

--- src/SortMain.java
+++ src/SortMain.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
+
 public class SortMain {
-    public static void main(String[] args) throws Exception {
-        // convert String[] to int[]
-        // create method to call Sorter with unsorted list
-        // save sorted list in variable
-        // crate method to print sorted list in console
+    public static void main(String[] args) {
+        int[] unsorted = Arrays.stream(args).mapToInt(Integer::parseInt).toArray();     
+        int [] sorted = Sorter.sort(unsorted);                                         
+        
+        for (int i : sorted) {                                                          
+           System.out.print(i + " ");
+        }
+        
     }
 }
