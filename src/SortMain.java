c8a859d7b6a19a4c3d793e0e6049be2b2382432b
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-11-14 14:53:49 +0100

    added code comments

--- src/SortMain.java
+++ src/SortMain.java
@@ -2,9 +2,10 @@ import java.util.Arrays;
 
 public class SortMain {
     public static void main(String[] args) {
-        int[] unsorted = Arrays.stream(args).mapToInt(Integer::parseInt).toArray();     
-        int [] sorted = Sorter.sort(unsorted);                                         
+        int[] unsorted = Arrays.stream(args).mapToInt(Integer::parseInt).toArray();     // converts args from String[] to int[]
+        int [] sorted = Sorter.sort(unsorted);                                          // variable to hold sorted array returned from sort method 
         
+        // print sorted array in terminal
         for (int i : sorted) {                                                          
            System.out.print(i + " ");
         }
--- src/Sorter.java
+++ src/Sorter.java
@@ -1,5 +1,8 @@
 public class Sorter {
 
+    // sorting method to sort int[] elements from smallest to biggest
+    // compares iterated element [i] with the next element [j] in the array
+    // elements switch place if [i] is bigger than [j] 
     public static int[] sort(int[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
