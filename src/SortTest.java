00c737aa201c0ed8a7dda557ba51fb6aa54b0e89
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-11-15 19:51:43 +0100

    new class for Sort testing. with sort examples

---
+++ src/SortTest.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class SortTest {
+    // 5 olika tester med olika testfall, negativa tal, 0 och positiva tal för att 
+    // säkerställa att er sorteringsalgoritm fungerar för alla möjliga listor av tal
+    String[] arrTest1 = {"76", "11", "-55", "2", "16", "-21", "7", "88"};
+    String[] arrTest2 = {"144", "34", "68", "0", "1", "919", "399", "8"};
+    String[] arrTest3 = {"-13", "-99", "-34", "-512", "-1", "-131"};
+    String[] arrTest4 = {"120.0", "411.0", "0.0", "-3.0", "10", "44"};
+    String[] arrTest5 = {"true", "false", "true", "false", "true", "false"};
+    
+    // 
+    private void test() {
+    
+    
+    
+    
+    
+    }
+    
+}
