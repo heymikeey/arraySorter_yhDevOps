d38f4e81ed1b6c8813847fbe2bbd8674e8e8669a
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-11-15 22:12:24 +0100

    console printouts added

--- src/SortTest.java
+++ src/SortTest.java
@@ -9,29 +9,42 @@ public class SortTest {
     }
 
     // 
-    private void runTest(String[] example) {
+    private void runTest(String[] example, String expected) {
         int[] toSort = typeConverter(example);
-        int[] sorted = Sorter.sort(toSort);
-        System.out.print(Arrays.toString(sorted));
+        String sorted = Arrays.toString(Sorter.sort(toSort));
+
+        System.out.println("Sample array: " + Arrays.toString(example));
+        System.out.println("Expected return value: " + expected);
+        System.out.println("Sorted value returned: " + sorted);
+
+        if (sorted.equals(expected)) {
+            System.out.println("Successful! Returned expected value.");
+
+        }
+        else {
+            System.out.println("Failed! Did not return expected value");
+        }
+        
 
     
     }
     
     public static void main(String[] args) {
         String[] array1 = {"76", "11", "-55", "2", "16", "-21", "7", "88"};
-        int[] array1_expected = {-55, -21, 2, 7, 11, 16, 76, 88};
+        int[] array1Expected = {-55, -21, 2, 7, 11, 16, 76, 88};
 
         String[] array2 = {"144", "34", "68", "0", "1", "919", "399", "8"};
-        String[] array3 = {"-13", "-99", "-34", "-512", "-1", "-131"};
+        int[] array2Expected = {0, 1, 8, 34, 68, 144, 399, 919};
 
+        String[] array3 = {"-13", "-99", "-34", "-512", "-1", "-131"};
+        int[] array3Expected = {-512, -131, -99, -34, -13, -1};
         
-        SortTest array1_test = new SortTest();
         SortTest array2_test = new SortTest();
         SortTest array3_test = new SortTest();
         SortTest array4_test = new SortTest();
         SortTest array5_test = new SortTest();
 
-        array1_test.runTest(array1);
+        array3_test.runTest(array3, Arrays.toString(array3Expected));
 
     }
     
