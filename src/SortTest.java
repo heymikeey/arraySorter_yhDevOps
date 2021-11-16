56cd5f0869d5b6ebbef9044606b96c4057364b60
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-11-16 11:24:14 +0100

    created a constructor to contain sample arrays instead of in main

--- src/SortTest.java
+++ src/SortTest.java
@@ -1,50 +1,70 @@
 import java.util.Arrays;
 
 public class SortTest {
-    // 5 olika tester med olika testfall, negativa tal, 0 och positiva tal för att 
-    // säkerställa att er sorteringsalgoritm fungerar för alla möjliga listor av tal
+    String[] positives;
+    String[] negatives;
+    String[] mixed;
+    String[] duplicates;
+    String[] single;
+    
+    int[] positivesSorted;
+    int[] negativesSorted;
+    int[] mixedSorted;
+    int[] duplicatesSorted;
+    int[] singleSorted;
+    
+    public SortTest() {
+        this.positives = new String[] {"144", "34", "68", "0", "1", "919", "399", "8"};
+        this.positivesSorted = new int[] {0, 1, 8, 34, 68, 144, 399, 919};
+        
+        this.negatives = new String[] {"-13", "-99", "-34", "-512", "-1", "-131"};
+        this.negativesSorted = new int[] {-512, -131, -99, -34, -13, -1};
+        
+        this.mixed = new String[] {"76", "11", "-55", "2", "16", "-21", "7", "88"};
+        this.mixedSorted = new int[] {-55, -21, 2, 7, 11, 16, 76, 88};
+        
+        this.duplicates = new String[] {"1", "0", "1", "0", "1", "0", "1", "0"};
+        this.duplicatesSorted = new int[] {0, 0, 0, 0, 1, 1, 1, 1}; 
+        
+        this.single = new String[] {"9"};
+        this.singleSorted = new int[] {9};
+    }
    
-    private int[] typeConverter(String[] args) {
+    private static int[] typeConverter(String[] args) {
         return Arrays.stream(args).mapToInt(Integer::parseInt).toArray();
     }
 
     // 
-    private void runTest(String[] example, String expected) {
+    private static void runTest(String[] example, String expected) {
         int[] toSort = typeConverter(example);
         String sorted = Arrays.toString(Sorter.sort(toSort));
 
         System.out.println("Sample array: " + Arrays.toString(example));
-        System.out.println("Expected return value: " + expected);
+        System.out.println("Expected sorted value: " + expected);
         System.out.println("Sorted value returned: " + sorted);
 
         if (sorted.equals(expected)) {
-            System.out.println("Successful! Returned expected value.");
+            System.out.println("Successful! Expected value returned.");
 
         }
         else {
             System.out.println("Failed! Did not return expected value");
         }
         
+        System.out.println(" ");
 
     
     }
     
     public static void main(String[] args) {
-        String[] array1 = {"76", "11", "-55", "2", "16", "-21", "7", "88"};
-        int[] array1Expected = {-55, -21, 2, 7, 11, 16, 76, 88};
 
-        String[] array2 = {"144", "34", "68", "0", "1", "919", "399", "8"};
-        int[] array2Expected = {0, 1, 8, 34, 68, 144, 399, 919};
+        SortTest test = new SortTest();
 
-        String[] array3 = {"-13", "-99", "-34", "-512", "-1", "-131"};
-        int[] array3Expected = {-512, -131, -99, -34, -13, -1};
-        
-        SortTest array2_test = new SortTest();
-        SortTest array3_test = new SortTest();
-        SortTest array4_test = new SortTest();
-        SortTest array5_test = new SortTest();
-
-        array3_test.runTest(array3, Arrays.toString(array3Expected));
+        SortTest.runTest(test.positives, Arrays.toString(test.positivesSorted));
+        SortTest.runTest(test.negatives, Arrays.toString(test.negativesSorted));
+        SortTest.runTest(test.mixed, Arrays.toString(test.mixedSorted));
+        SortTest.runTest(test.duplicates, Arrays.toString(test.duplicatesSorted));
+        SortTest.runTest(test.single, Arrays.toString(test.singleSorted));
 
     }
     
